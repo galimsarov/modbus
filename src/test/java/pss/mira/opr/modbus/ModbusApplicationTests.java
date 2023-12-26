f0d0de6efe294561d5ac08614679edc7c045b845
galimsarov <galimsarov@pss.ru>
2023-12-26 10:49:21 +0500

    Initial commit

---
+++ src/test/java/pss/mira/opr/modbus/ModbusApplicationTests.java
@@ -0,0 +1,13 @@
+package pss.mira.opr.modbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
